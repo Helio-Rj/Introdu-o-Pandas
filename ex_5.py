9dbf2e4ea125fd011d53f5646616eba3eb6e4ccb
Hélio do Nascimento <115810349+Puritoka@users.noreply.github.com>
2024-04-17 17:56:24 -0300

    Aprendizado e adição de novas linhas e colunas com 2 métodos

---
+++ ex_5.py
@@ -0,0 +1,25 @@
+import pandas as pd
+
+#  from IPython.core.display_functions import display
+#  se quizer usar o 'display'
+
+# importar a base de dados
+tabela_vendas = pd.read_excel('Vendas.xlsx')
+
+# visualizar a base de dados
+pd.set_option('display.max_columns', None)
+
+#  Adicionando coluna nova a tabela a partir das que existem
+tabela_vendas['Comissão'] = tabela_vendas['Valor Final'] * 0.5
+
+#  Adicionando uma nova coluna a tabela com valor zerado
+tabela_vendas.loc[:, 'Imposto'] = 0
+
+#  Exibição de lojas/quantidade
+lojas = tabela_vendas[['ID Loja', 'Quantidade']].groupby('ID Loja').sum()
+
+print(lojas.head(6))  # pode se escolher o N° de linhas para exibição em ordem crescente
+print(lojas.shape)  # mostra quantas linhas e colunas a tabela tem
+print(lojas.describe())  # Descrição completa para análise de dados
+print(tabela_vendas.loc[1])  # Exibe uma linha pelo índice
+print(tabela_vendas)
